9d43b65e99ac8573549731c5c65067fc841adc8a
Aaron <aaron@zxinsight.com>
2015-12-29 11:50:26 +0800

    init
    init

---
+++ app/src/main/java/com/mw/grab/LuckyMoneyService.java
@@ -0,0 +1,208 @@
+package com.mw.grab;
+
+import android.accessibilityservice.AccessibilityService;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.os.Build;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class LuckyMoneyService extends AccessibilityService {
+    private List<AccessibilityNodeInfo> mReiceiveNode = null;
+    private List<AccessibilityNodeInfo> mUnpackNode = null;
+
+    private boolean mLuckyMoneyPicked;
+    private boolean mLuckyMoneyReceived;
+    private boolean mNeedUnpack;
+    private boolean mNeedBack = false;
+    private List<String> fetchIdentifiers = new ArrayList<>();
+    private String lastFetchedHongbaoId = null;
+    private long lastFetchedtime = System.currentTimeMillis();
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+        final int eventType = event.getEventType();
+        Log.e("aaron", "eventType ="+eventType);
+
+        if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+
+            List<CharSequence> texts = event.getText();
+            Log.e("aaron", "texts ="+texts);
+            for (CharSequence t : texts) {
+
+                if (t.toString().contains("[微信红包]")) {// 获取通知栏字符，若包含 [微信红包]
+                    // 则模拟手指点击事件
+                    Log.e("aaron", "通知栏[微信红包]");
+                    handleNotificationChange(event);
+                    break;
+                }
+            }
+
+        }
+
+        if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED || eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
+
+            AccessibilityNodeInfo nodeInfo = event.getSource();
+
+            if (null != nodeInfo) {
+                mReiceiveNode = null;
+                mUnpackNode = null;
+                checkNodeInfo();
+                /* 如果已经接收到红包并且还没有戳开 */
+                if (mLuckyMoneyReceived && !mLuckyMoneyPicked && (mReiceiveNode != null)) {
+                    int size = mReiceiveNode.size();
+                    if (size > 0) {
+
+                        String id = getHongbaoHash(mReiceiveNode.get(size - 1));
+
+                        long now = System.currentTimeMillis();
+                        if (id == null || (now - lastFetchedtime < 5000) && id.equals(lastFetchedHongbaoId))
+                            return;
+
+                        lastFetchedHongbaoId = id;
+                        lastFetchedtime = now;
+
+                        AccessibilityNodeInfo cellNode = mReiceiveNode.get(size - 1);
+                        cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        mLuckyMoneyReceived = false;
+                        mLuckyMoneyPicked = true;
+                    }
+                }
+                /* 如果戳开但还未领取 */
+                if (mNeedUnpack && (mUnpackNode != null)) {
+                    int size = mUnpackNode.size();
+                    if (size > 0) {
+                        AccessibilityNodeInfo cellNode = mUnpackNode.get(size - 1);
+                        cellNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        mNeedUnpack = false;
+                    }
+                }
+            }
+
+            if (mNeedBack) {
+                performGlobalAction(GLOBAL_ACTION_BACK);
+                mNeedBack = false;
+            }
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    /**
+     * 检查节点信息
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void checkNodeInfo() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+
+        if (nodeInfo != null) {
+            /* 聊天会话窗口，遍历节点匹配“领取红包” */
+            List<AccessibilityNodeInfo> node1 = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
+            if (!node1.isEmpty()) {
+                String nodeId = Integer.toHexString(System.identityHashCode(nodeInfo));
+                Log.d("233", nodeInfo.toString());
+                if (!checkFetched(nodeId)) {
+                    mLuckyMoneyReceived = true;
+                    mReiceiveNode = node1;
+                }
+                return;
+            }
+
+            /* 戳开红包，红包还没抢完，遍历节点匹配“拆红包” */
+            List<AccessibilityNodeInfo> node2 = nodeInfo.findAccessibilityNodeInfosByText("拆红包");
+            if (!node2.isEmpty()) {
+                mUnpackNode = node2;
+                mNeedUnpack = true;
+                return;
+            }
+
+            /* 戳开红包，红包已被抢完，遍历节点匹配“红包详情”和“手慢了” */
+            if (mLuckyMoneyPicked) {
+                List<AccessibilityNodeInfo> node3 = nodeInfo.findAccessibilityNodeInfosByText("红包详情");
+                List<AccessibilityNodeInfo> node4 = nodeInfo.findAccessibilityNodeInfosByText("手慢了");
+                if (!node3.isEmpty() || !node4.isEmpty()) {
+                    mNeedBack = true;
+                    mLuckyMoneyPicked = false;
+                }
+            }
+        }
+
+    }
+
+
+    private void handleNotificationChange(AccessibilityEvent event) {
+        try {
+
+            Notification notification = (Notification) event
+                    .getParcelableData();
+
+            PendingIntent pendingIntent = notification.contentIntent;
+
+            pendingIntent.send(); // 点击通知栏消息
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean checkFetched(String nodeId) {
+        for (String identifier : fetchIdentifiers) {
+            if (nodeId.equals(identifier))
+                return true;
+        }
+        fetchIdentifiers.add(nodeId);
+        return false;
+    }
+
+    /**
+     * 获取节点对象唯一的id，通过正则表达式匹配
+     * AccessibilityNodeInfo@后的十六进制数字
+     *
+     * @param node AccessibilityNodeInfo对象
+     * @return id字符串
+     */
+    private String getNodeId(AccessibilityNodeInfo node) {
+        /* 用正则表达式匹配节点Object */
+        Pattern objHashPattern = Pattern.compile("(?<=@)[0-9|a-z]+(?=;)");
+        Matcher objHashMatcher = objHashPattern.matcher(node.toString());
+
+        // AccessibilityNodeInfo必然有且只有一次匹配，因此不再作判断
+        objHashMatcher.find();
+
+        return objHashMatcher.group(0);
+    }
+
+    /**
+     * 将节点对象的id和红包上的内容合并
+     * 用于表示一个唯一的红包
+     *
+     * @param node 任意对象
+     * @return 红包标识字符串
+     */
+    private String getHongbaoHash(AccessibilityNodeInfo node) {
+        /* 获取红包上的文本 */
+        String content;
+        try {
+            AccessibilityNodeInfo i = node.getParent().getChild(0);
+            content = i.getText().toString();
+        } catch (NullPointerException npr) {
+            return null;
+        }
+
+        return content + "@" + getNodeId(node);
+    }
+}
---
+++ app/src/main/java/com/mw/grab/MainActivity.java
@@ -0,0 +1,100 @@
+package com.mw.grab;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private final Intent mAccessibleIntent =
+            new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+
+    private Button switchPlugin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchPlugin = (Button) findViewById(R.id.button_accessible);
+
+        handleMIUIStatusBar();
+        updateServiceStatus();
+    }
+
+    private void handleMIUIStatusBar() {
+        Window window = getWindow();
+
+        Class clazz = window.getClass();
+        try {
+            int tranceFlag = 0;
+            int darkModeFlag = 0;
+            Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_TRANSPARENT");
+            tranceFlag = field.getInt(layoutParams);
+
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(window, tranceFlag, tranceFlag);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateServiceStatus();
+    }
+
+    @Override
+    protected void onDestroy() {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        super.onDestroy();
+    }
+
+    private void updateServiceStatus() {
+        boolean serviceEnabled = false;
+
+        AccessibilityManager accessibilityManager =
+                (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> accessibilityServices =
+                accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().equals(getPackageName() + "/.LuckyMoneyService")) {
+                serviceEnabled = true;
+                break;
+            }
+        }
+
+        if (serviceEnabled) {
+            switchPlugin.setText("关闭插件");
+            // Prevent screen from dimming
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            switchPlugin.setText("开启插件");
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    public void onButtonClicked(View view) {
+        startActivity(mAccessibleIntent);
+    }
+
+    public void openGithub(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/snailflying/grab_money"));
+        startActivity(browserIntent);
+    }
+}
